f33c633927acb38c3cbff23314c269cf9bfa0c8d
EmilyVargas <92656230+EmilyVargas@users.noreply.github.com>
2024-10-10 12:37:02 -0600

    Strapi Implementado, Cards funcionando junto con el filtro
    Implementacion de informacion de cada proyecto en proceso

--- src/components/projects-filter/ProjectsFilter.jsx
+++ src/components/projects-filter/ProjectsFilter.jsx
@@ -1,48 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './ProjectsFilter.css';
-import Menu from './Menu';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import {useNavigate } from 'react-router-dom';
 
 const ProjectsFilter = () => {
-    const [items, setItems] = useState(Menu);
-    const filterItem = (categoryItem) => {
-        const updatedItems = Menu.filter((curElem) => {
-            return curElem.category === categoryItem;
-        })
-        setItems(updatedItems);
-    }
-    const [selectedCategory, setSelectedCategory] = useState('Todos');
 
+    const [proyectos, setProyectos] = useState([]);
+    const [originalProyectos, setOriginalProyectos] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('Todos');
     const navigate=useNavigate();
 
+    useEffect(() => {
+        const fetchProyectos = async () => {
+            try {
+                const response = await axios.get('http://localhost:1337/api/proyectos?populate=*');
+                setProyectos(response.data.data);
+                setOriginalProyectos(response.data.data);
+            }catch(error){
+                console.error('Error fetching projects', error);
+            }
+        };
+        fetchProyectos();
+    }, []);
+
+    
+    // Función para filtrar los proyectos según la categoría
+    const filterProy = (categoryItem) => {
+        if (categoryItem === 'Todos') {
+            setProyectos(originalProyectos); // Mostrar todos los proyectos
+        } else {
+            const updatedItems = originalProyectos.filter((curElem) => {
+                return curElem.category === categoryItem;
+            });
+            setProyectos(updatedItems);
+        }
+        setSelectedCategory(categoryItem);
+    };
+
     return (
-        <section className='work container section' id='work'>
+        <section className='work container section'>
             <h2 className='title-project'>Proyectos</h2>
             <p className='p-project'>Aqui encontraras proyectos realizados tanto en solitario como en equipo.</p>
             <div className='work-filters'>
                 <span
                     className={`work-item ${selectedCategory === 'Todos' ? 'selected' : ''}`}
-                    onClick={() => { setItems(Menu); setSelectedCategory('Todos'); }}
+                    onClick={() => { filterProy('Todos'); setSelectedCategory('Todos'); }}
                 >Todos</span>
                 <span
                     className={`work-item ${selectedCategory === 'Frontend' ? 'selected' : ''}`}
-                    onClick={() => { filterItem('Frontend'); setSelectedCategory('Frontend'); }}
+                    onClick={() => { filterProy('Frontend'); setSelectedCategory('Frontend'); }}
                 >FrontEnd</span>
                 <span
                     className={`work-item ${selectedCategory === 'UX' ? 'selected' : ''}`}
-                    onClick={() => { filterItem('UX'); setSelectedCategory('UX'); }}
+                    onClick={() => { filterProy('UX'); setSelectedCategory('UX'); }}
                 >UX</span>
             </div>
 
             <div className="work-container grid">
-                {items.map((elem) => {
-                    const { id, image, workstation, title, description, category } = elem;
+                {proyectos.map((elem) => {
+                    const {id, workstation, title, summary, category, image } = elem;
+                    const imageUrl = image && image.length > 0 && image[0].url
+          ? `http://localhost:1337${image[0].url}` // Accede al primer elemento del array
+          : 'URL_POR_DEFECTO'; // Usa una URL por defecto si no hay imagen
                     return (
                         <div className="card-container" key={id}>
                             <div className="image-container">
-                                <img src={image} alt="" />
+                                <img src={imageUrl} alt={title} />
                             </div>
                             <span className='card-category'>{category}</span>
                             <div className="info-container">
@@ -53,7 +78,7 @@ const ProjectsFilter = () => {
                                     {title}
                                 </div>
                                 <div className="card-description">
-                                    {description}
+                                    {summary}
                                 </div>
                                 <button className='card-button' onClick={()=>{const path=category=='UX'?'/pageux':'/pageweb'; navigate(path, {id:id});}}>Mas informacion <FontAwesomeIcon icon={faArrowRight}/></button>
                             </div>
